c909caa85c66a066c48360dd5d5f9f6c1373dd3f
idandmon-arch <idan.dmon@gmail.com>
2026-07-12 15:46:16 +0100

    feat: implement premium arcade football match engine with advanced collisions and set pieces

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import network_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
 }
